fa3edb21d1e458d84a821f40b957be180c20feda
Talwind1 <talwind1@gmail.com>
2021-12-30 22:18:51 +0200

    check

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  console.log("hiiiiiiiiiiiiiiiiiii");
-  return <div className="App"></div>;
+  let arr = [1, 2, 3];
+
+  const show = () => {
+    arr.forEach((i) => {
+      i = i + 1;
+    });
+  };
+  return <div className="App">{show()}</div>;
 }
 
 export default App;
