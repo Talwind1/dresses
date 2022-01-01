05d0ec67e5dc14b78ab223e3c061fa3111c03706
Talwind1 <talwind1@gmail.com>
2022-01-01 06:55:34 +0200

    display

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import Dresses from "./components/Dresses";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      Borrow a dress App
+      <Dresses></Dresses>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Dresses.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+function Dresses() {
+  const [dresses, setDresses] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    const fetching = async () => {
+      try {
+        const { data } = await axios.get(
+          "https://61c311049cfb8f0017a3e937.mockapi.io/dresses"
+        );
+        setDresses(data);
+        setLoading(false);
+      } catch (e) {
+        throw e.messege;
+      }
+    };
+    fetching();
+  }, []);
+
+  const display = () => {
+    return dresses.map((dress) => {
+      return (
+        <div key={dress.id} className="dress">
+          <h4>{dress.location}</h4>
+          <img src={dress.image} alt="dress pic"></img>
+          <h4>{dress.size}</h4>
+          <h4>{dress.color}</h4>
+        </div>
+      );
+    });
+  };
+  return (
+    <div className="dresses">
+      {loading && <h3>Loading...</h3>}
+      {dresses && display()}
+    </div>
+  );
+}
+export default Dresses;
